7155dece9fd144b94610ab51e6295bc14cb14e17
Robocotik <alexander.starkin@gmail.com>
2025-04-19 21:07:13 +0300

    feat

--- nosqllife/src/components/AboutPage/index.tsx
+++ nosqllife/src/components/AboutPage/index.tsx
@@ -4,9 +4,16 @@ import type { FC } from 'react'
 import Props from './AboutPage.props'
 import { PageLayout } from '@/layouts/PageLayout'
 export const AboutPage:FC<Props> = ({className}) => {
-  return(
-    <PageLayout className={`${className}`}>
-
+  return (
+    <PageLayout className={`h-dvh ${className}`}>
+      <div className='w-full h-full flex justify-center items-center'>
+        <p>
+          Рады приветсвовать на нашем сайте 
+          Мы приложение помощник
+          Готовы оказать любую помощь с
+          вашими отходами и расчетами
+        </p>
+      </div>
     </PageLayout>
-  )
+  );
 }
