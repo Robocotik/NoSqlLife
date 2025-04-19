e364a9932a174e9f45637125d581487d1799ac1c
Robocotik <alexander.starkin@gmail.com>
2025-04-19 21:07:13 +0300

    feat

--- nosqllife/src/app/page.tsx
+++ nosqllife/src/app/page.tsx
@@ -2,10 +2,12 @@
 import {PageLayout} from '@/layouts/PageLayout';
 import {Header} from '@/components/Header';
 import {CalculatorPage} from '@/components/CalculatorPage';
+import { AboutPage } from '@/components/AboutPage';
 export default function Home() {
   return (
     <div className='flex flex-col w-full'>
       <Header />
+      <AboutPage />
       <CalculatorPage />
     </div>
   );
