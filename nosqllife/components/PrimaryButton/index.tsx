6169638cf5c20edc8150899bd5550c6c3eef0922
Robocotik <alexander.starkin@gmail.com>
2025-04-19 12:42:43 +0300

    [feat] main page: add done login page

--- nosqllife/components/PrimaryButton/index.tsx
+++ nosqllife/components/PrimaryButton/index.tsx
@@ -2,5 +2,10 @@ import type {FC} from 'react';
 import type Props from './PrimaryButton.props';
 
 export const PrimaryButton: FC<Props> = ({children, className}) => {
-  return <button className={`${className} rounded-2xl w-full h-12 flex items-center justify-center`}>{children}</button>;
+  return (
+    <button
+      className={`${className} cursor-pointer rounded-2xl w-full py-1 flex items-center justify-center`}>
+      {children}
+    </button>
+  );
 };
