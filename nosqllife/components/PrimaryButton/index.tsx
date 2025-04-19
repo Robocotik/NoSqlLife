e9bd29f60244fd3d5691093bb858f6c4c9031434
Robocotik <alexander.starkin@gmail.com>
2025-04-19 13:07:53 +0300

    [feat] page: add login page

--- nosqllife/components/PrimaryButton/index.tsx
+++ nosqllife/components/PrimaryButton/index.tsx
@@ -1,9 +1,11 @@
 import type {FC} from 'react';
 import type Props from './PrimaryButton.props';
 
-export const PrimaryButton: FC<Props> = ({children, className}) => {
+export const PrimaryButton: FC<Props> = ({children,type, className, ...props}) => {
   return (
     <button
+      {...props}
+      type={type}
       className={`${className} cursor-pointer rounded-2xl w-full py-1 flex items-center justify-center`}>
       {children}
     </button>
--- nosqllife/src/app/login/page.tsx
+++ nosqllife/src/app/login/page.tsx
@@ -1,50 +1,97 @@
+'use client';
 import {PageLayout} from '../../../layouts/PageLayout';
 import {PrimaryButton} from '../../../components/PrimaryButton';
 import reg_img from '@/assets/img/bg_reg.jpg';
+import * as yup from 'yup'; // Импортируем yup
+import {useFormik} from 'formik'; // Импортируем useFormik из formik
+const validationSchema = yup.object().shape({
+  email: yup.string().email('Неверный формат email').required('Email обязателен'),
+  password: yup
+    .string()
+    .min(6, 'Пароль должен содержать минимум 6 символов')
+    .required('Пароль обязателен'),
+});
+interface FormValues {
+  email: string;
+  password: string;
+  organization: boolean;
+}
+
 export default function Login() {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      organization: false,
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values: FormValues) => {
+      console.log(values);
+    },
+  });
   return (
     <PageLayout className='h-dvh my-auto' image={reg_img.src}>
       <div className='w-full h-full flex justify-center items-center'>
-        <div className='flex flex-col justify-between items-stretch gap-4 w-96 h-72 p-12 text-black rounded-2xl border-2 border-white bg-white/50'>
+        <form
+          onSubmit={formik.handleSubmit}
+          className='flex flex-col justify-between items-stretch gap-4 w-96 h-72 p-12 text-black rounded-2xl border-3 shadow-2xl border-main_green/80 bg-white/50'>
           <div className='flex flex-col w-full h-full justify-start gap-4 items-start'>
-            <div className='flex gap-2 w-full '>
-              <label htmlFor='email' className='w-24'>
-                email
-              </label>
-              <input
-                type='email'
-                placeholder='email'
-                id='email'
-                className='ps-2 rounded-md border border-white/40'
-              />
+            <div className='flex flex-col gap-2 w-full'>
+              <div className='flex gap-2 w-full'>
+                <label htmlFor='email' className='w-24'>
+                  email
+                </label>
+                <input
+                  type='email'
+                  id='email'
+                  className='ps-2 rounded-md border border-white/40'
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                />
+              </div>
+
+              {formik.touched.email && formik.errors.email ? (
+                <div className='text-red-500'>{formik.errors.email}</div>
+              ) : null}
             </div>
-            <div className='flex gap-2 w-full'>
-              <label htmlFor='password' className='w-24'>
-                password
-              </label>
-              <input
-                placeholder='password'
-                type='password'
-                id='password'
-                className='ps-2 rounded-md border border-white/40'
-              />
+
+            <div className='flex flex-col gap-2 w-full'>
+              <div className='flex gap-2 w-full'>
+                <label htmlFor='password' className='w-24'>
+                  password
+                </label>
+                <input
+                  type='password'
+                  id='password'
+                  className='ps-2 rounded-md border border-white/40'
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                />
+              </div>
+
+              {formik.touched.password && formik.errors.password ? (
+                <div className='text-red-500'>{formik.errors.password}</div>
+              ) : null}
             </div>
             <div className='flex items-center mt-4'>
               <input
-                defaultChecked
-                id='checked-checkbox'
+                id='organization'
                 type='checkbox'
-                value=''
+                checked={formik.values.organization}
+                onChange={formik.handleChange}
                 className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
               />
-              <label htmlFor='checked-checkbox' className='ms-2 text-sm font-medium text-gray-900 '>
+              <label htmlFor='organization' className='ms-2 text-sm font-medium text-gray-900'>
                 У меня есть организация
               </label>
             </div>
           </div>
-
-          <PrimaryButton className='bg-main_green'>Зарегистрироваться</PrimaryButton>
-        </div>
+          <PrimaryButton className='bg-main_green' type='submit'>
+            Зарегистрироваться
+          </PrimaryButton>
+        </form>
       </div>
     </PageLayout>
   );
